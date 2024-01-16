0ab3f73858a6c406928e1aaa4f44915b49705f63
Mirkocole <mirkocole92@gmail.com>
2024-01-16 21:03:48 +0100

    Consegna Extra

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Funzione Carosello
 function Carousel(id){
     let items = document.querySelectorAll(id+'.carousel .carousel-item');
 
@@ -22,4 +23,33 @@ let carousels =  document.querySelectorAll('.carousel.slide');
 for (const carousel of carousels) {
     let id = '#'+carousel.id;
     Carousel(id);
+}
+
+// Funzione Fadein EXTRA
+
+let containers = document.querySelectorAll('.container-fluid');
+let maxView = window.innerHeight - (window.innerHeight /5) ;
+for (const container of containers) {
+    
+    FadeIn(container);
+}
+
+document.addEventListener('scroll',()=>{
+    let containers = document.querySelectorAll('.container-fluid');
+    for (const container of containers) {
+        FadeIn(container);
+    }
+})
+
+
+
+
+function FadeIn(div){
+    let maxView = window.innerHeight - (window.innerHeight /5) ;
+    if(div.getBoundingClientRect().top > maxView){
+        div.style.animation = 'FadeOut 0.5s ease-in forwards';
+    }else{
+        // div.style.opacity = '1';
+        div.style.animation = 'FadeIn 1s linear forwards';
+    }
 }
